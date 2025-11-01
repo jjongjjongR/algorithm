08bc9e3ed3c897d1f6669ebcbf0dbdbf2b1ef173
jjongjjongR <127107111+jjongjjongR@users.noreply.github.com>
2025-11-02 02:23:32 +0900

    [Bronze V] Title: N 찍기, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+input = sys.stdin.readline
+
+def main():
+    A = int(input().strip())
+
+    for a in range(1,A+1):
+        print(a)
+
+if __name__ == "__main__":
+    main()
